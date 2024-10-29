cf7093791fac028b01ae437880405f4e37c9a8a3
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-10-29 15:58:06 +0300

    merge branch feat/scheduled_cleaning_absentee_data into dev: scheduled cleaning table absent

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from aiogram.utils import executor
 import handlers  # noqa
 from loader import dp
 from tasks.mark_absents_mailing import Mailer
+from tasks.scheduled_cleaning_tables import Cleaner
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
+
 	mailer = Mailer()
+	cleaner = Cleaner()
+
 	loop.create_task(mailer.start())
+	loop.create_task(cleaner.start())
+
 	executor.start_polling(dp)
---
+++ tasks/scheduled_cleaning_tables.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from database import crud
+from bot_logging import logger
+
+
+class Cleaner:
+	INTERVAL_IN_SECONDS = 60 * 60 * 24 * 7  # Одна неделя
+
+	@staticmethod
+	async def clean_table_absent():
+		crud.table_absent.clean_table()
+
+	async def start(self):
+		while True:
+			logger.info('Очищаем таблицу Absent')
+			try:
+				await self.clean_table_absent()
+			except Exception as exc:
+				raise exc
+			else:
+				logger.info('Таблица Absent очищена')
+			await asyncio.sleep(self.INTERVAL_IN_SECONDS)
