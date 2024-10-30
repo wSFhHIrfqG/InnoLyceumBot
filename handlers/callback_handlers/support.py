0046390772436672beacb4f00f717615197d6d87
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-10-30 22:46:03 +0400

    feat: non-registered users cant use support command

--- handlers/callback_handlers/support.py
+++ handlers/callback_handlers/support.py
@@ -5,23 +5,15 @@ from aiogram import types
 import keyboards.inline.support
 from loader import bot, dp
 from states.user_states import UserStates
-from database import crud
 
 
 @dp.callback_query_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), text='support', state='*')
 async def support(call: types.CallbackQuery, state: FSMContext):
+	await state.set_state(UserStates.support_wait_message)
+
 	await call.message.delete()
-
-	if crud.table_employee.get_employee_by_telegram_id(telegram_id=call.from_user.id):
-		await state.set_state(UserStates.support_wait_message)
-
-		await bot.send_message(
-			chat_id=call.from_user.id,
-			text='Введите текст',
-			reply_markup=keyboards.reply.support.cancel_markup()
-		)
-	else:
-		await bot.send_message(
-			chat_id=call.from_user.id,
-			text='Вы не зарегистрированы'
-		)
+	await bot.send_message(
+		chat_id=call.from_user.id,
+		text='Введите текст',
+		reply_markup=keyboards.reply.support.cancel_markup()
+	)
--- handlers/command_handlers/support.py
+++ handlers/command_handlers/support.py
@@ -3,13 +3,20 @@ from aiogram.dispatcher import FSMContext
 from aiogram import types
 
 from loader import bot, dp
+from database import crud
 import keyboards
 
 
 @dp.message_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), commands=['support'], state='*')
 async def support(message: types.Message, state=FSMContext):
-	await bot.send_message(
-		chat_id=message.from_user.id,
-		text='Здесь вы можете оставить свои идеи и предложения',
-		reply_markup=keyboards.inline.support.write_support_message_markup()
-	)
+	if crud.table_employee.get_employee_by_telegram_id(telegram_id=message.from_user.id):
+		await bot.send_message(
+			chat_id=message.from_user.id,
+			text='Здесь вы можете оставить свои идеи и предложения',
+			reply_markup=keyboards.inline.support.write_support_message_markup()
+		)
+	else:
+		await bot.send_message(
+			chat_id=message.from_user.id,
+			text='Вы не зарегистрированы'
+		)
